b9a0f014776c5cc5bc935cede6cd2c6575b45d86
ilosrim <mirsolikhdev@gmail.com>
2021-11-11 22:42:25 +0500

    expressjs example

---
+++ lesson-09/members.js
@@ -0,0 +1,24 @@
+// middleware
+const members = [{
+    id: 1,
+    name: 'ilosrim',
+    age: 23
+  },
+  {
+    id: 2,
+    name: 'sherali',
+    age: 15
+  },
+  {
+    id: 3,
+    name: 'hoshim',
+    age: 30
+  },
+  {
+    id: 4,
+    name: 'toshmat',
+    age: 17
+  },
+];
+
+module.exports = members;
